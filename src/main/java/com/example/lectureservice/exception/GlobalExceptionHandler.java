b72750048e1b61705ea585102d12a6bc7c31c98c
owner <toltolyibh@gmail.com>
2024-06-26 13:21:54 +0900

    exception handler 추가

---
+++ src/main/java/com/example/lectureservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.lectureservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
